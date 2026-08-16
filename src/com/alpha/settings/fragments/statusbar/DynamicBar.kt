21911d02cde23c72f45453403abc196b6507f644
elpaablo <donopwhile1@gmail.com>
2026-08-17 00:42:06 +0100

    AlphaSettings: drop the lockscreen battery chip mode

--- src/com/alpha/settings/fragments/statusbar/DynamicBar.kt
+++ src/com/alpha/settings/fragments/statusbar/DynamicBar.kt
@@ -57,7 +57,6 @@ class DynamicBar : SettingsPreferenceFragment() {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.dynamic_bar)
 
-        setupKeyguardSubPrefs()
         setupEventToggles()
         updateCompactNotificationVisibility()
         registerObserver()
@@ -66,23 +65,6 @@ class DynamicBar : SettingsPreferenceFragment() {
     override fun onDestroy() {
         super.onDestroy()
         settingsObserver?.let { resolver.unregisterContentObserver(it) }
-    }
-
-    private fun setupKeyguardSubPrefs() {
-        val keyguardPref = findPreference<SwitchPreferenceCompat>(SETTINGS_KEY_KEYGUARD_ENABLED)
-        keyguardPref?.setOnPreferenceChangeListener { _, newValue ->
-            updateKeyguardSubPrefsVisibility(newValue as Boolean)
-            true
-        }
-        // Set initial visibility from current setting value
-        val keyguardEnabled = Settings.Secure.getIntForUser(
-            resolver, SETTINGS_KEY_KEYGUARD_ENABLED, 1, UserHandle.USER_CURRENT
-        ) == 1
-        updateKeyguardSubPrefsVisibility(keyguardEnabled)
-    }
-
-    private fun updateKeyguardSubPrefsVisibility(keyguardEnabled: Boolean) {
-        findPreference<Preference>(SETTINGS_KEY_BATTERY_CHIP_MODE)?.isVisible = keyguardEnabled
     }
 
     private fun setupEventToggles() {
@@ -144,22 +126,12 @@ class DynamicBar : SettingsPreferenceFragment() {
                         }
                         updateCompactNotificationVisibility()
                     }
-                    SETTINGS_KEY_KEYGUARD_ENABLED -> {
-                        val enabled = Settings.Secure.getIntForUser(
-                            resolver, SETTINGS_KEY_KEYGUARD_ENABLED, 1, UserHandle.USER_CURRENT
-                        ) == 1
-                        updateKeyguardSubPrefsVisibility(enabled)
-                    }
                 }
             }
         }
 
         resolver.registerContentObserver(
             Settings.Secure.getUriFor(SETTINGS_KEY_EVENTS),
-            false, settingsObserver!!
-        )
-        resolver.registerContentObserver(
-            Settings.Secure.getUriFor(SETTINGS_KEY_KEYGUARD_ENABLED),
             false, settingsObserver!!
         )
     }
@@ -173,7 +145,6 @@ class DynamicBar : SettingsPreferenceFragment() {
         private const val SETTINGS_KEY_KEYGUARD_ENABLED = "ax_dynamic_bar_keyguard_enabled"
         private const val SETTINGS_KEY_EVENTS = "ax_dynamic_bar_events"
         private const val SETTINGS_KEY_COMPACT_NOTIFICATIONS = "ax_dynamic_bar_compact_notifications"
-        private const val SETTINGS_KEY_BATTERY_CHIP_MODE = "ax_dynamic_bar_keyguard_battery_chip_mode"
 
         // Cutout ring settings (Show on cutout)
         private const val SETTINGS_KEY_CUTOUT_RING_ENABLED = "cutout_ring_enabled"
@@ -199,10 +170,6 @@ class DynamicBar : SettingsPreferenceFragment() {
             )
             Settings.Secure.putIntForUser(
                 resolver, SETTINGS_KEY_COMPACT_NOTIFICATIONS, 1,
-                UserHandle.USER_CURRENT
-            )
-            Settings.Secure.putIntForUser(
-                resolver, SETTINGS_KEY_BATTERY_CHIP_MODE, 1,
                 UserHandle.USER_CURRENT
             )
             // Reset cutout ring settings
