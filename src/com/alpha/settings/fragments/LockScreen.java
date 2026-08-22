f4babe229ea257a12c44b875e6b7604305bfb66a
elpaablo <donopwhile1@gmail.com>
2026-08-22 16:15:21 +0100

    AlphaSettings: add the Now Playing cloud fallback toggle

--- src/com/alpha/settings/fragments/LockScreen.java
+++ src/com/alpha/settings/fragments/LockScreen.java
@@ -148,6 +148,8 @@ public class LockScreen extends SettingsPreferenceFragment
         Settings.Secure.putIntForUser(resolver,
                 "quick_look_now_playing", 1, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
+                "quick_look_now_playing_cloud", 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
                 "quick_look_google_smartspace", 1, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 "quick_look_smartspacer", 1, UserHandle.USER_CURRENT);
